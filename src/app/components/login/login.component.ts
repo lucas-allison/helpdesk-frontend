e77daac91c0e906a9f8c80bedf184d3a20d9ce56
lucas-allison <lucas.allison444@gmail.com>
2025-10-27 17:06:52 -0300

    Valida;áo de formulários para LOGIN

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Credenciais } from '../../models/credenciais';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  creds: Credenciais = {
+    email: '',
+    senha: '',
+  };
 
+  email = new FormControl(null, Validators.email);
+  senha = new FormControl(null, Validators.minLength(3));
+
+  validaCampos(): boolean {
+    return this.email.valid && this.senha.valid
+  }
 }
