6179e03c39f5336ac07811692db307ba381b6174
lucas-allison <lucas.allison444@gmail.com>
2025-10-27 16:05:20 -0300

    Rota para TECNICO-LIST

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
+import { TecnicoListComponent } from './components/tecnico-list/tecnico-list.component';
 
 const routes: Routes = [
   {
     path: '',
     component: NavComponent,
-    children: [{ path: 'home', component: HomeComponent }],
+    children: [
+      { path: 'home', component: HomeComponent },
+      { path: 'tecnicos', component: TecnicoListComponent }
+    ],
   },
 ];
 
