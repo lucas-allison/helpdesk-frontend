2ab658eaf4268dad6e9b3c816ca6608a8afccfc3
lucas-allison <lucas.allison444@gmail.com>
2025-10-28 14:58:42 -0300

    Finalizando componente TECNICO-LIST

--- src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
+++ src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -1,39 +1,40 @@
-import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, ViewChild, AfterViewInit, OnInit } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { Tecnico } from '../../../models/tecnico';
+import { TecnicoService } from '../../../services/tecnico.service';
 
 @Component({
   selector: 'app-tecnico-list',
   templateUrl: './tecnico-list.component.html',
   styleUrls: ['./tecnico-list.component.css'],
 })
-export class TecnicoListComponent implements AfterViewInit {
-  ELEMENT_DATA: Tecnico[] = [
-    {
-      id: 1,
-      nome: 'Matheus',
-      cpf: '123.456.789-00',
-      email: 'teste@teste.com',
-      senha: '12345',
-      perfis: ['0'],
-      dataCriacao: '20/10/2022',
-    },
-  ];
 
-  displayedColumns: string[] = [
-    'id',
-    'nome',
-    'cpf',
-    'email',
-    'acoes',
-  ];
+export class TecnicoListComponent implements OnInit {
+  ELEMENT_DATA: Tecnico[] = [];
+
+  displayedColumns: string[] = ['id', 'nome', 'cpf', 'email', 'acoes'];
 
   dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
-  ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
+  constructor(private service: TecnicoService) {}
+
+  ngOnInit(): void {
+    this.findAll();
+  }
+
+  findAll(): void {
+    this.service.findAll().subscribe((resposta) => {
+      this.ELEMENT_DATA = resposta;
+      this.dataSource = new MatTableDataSource<Tecnico>(resposta);
+      this.dataSource.paginator = this.paginator;
+    });
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 }
