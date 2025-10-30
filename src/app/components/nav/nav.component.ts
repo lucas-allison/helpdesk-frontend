caf0ec9e44bcdf58f805ce1434369300fd92941a
lucas-allison <lucas.allison444@gmail.com>
2025-10-30 17:30:28 -0300

    Ajustes no CSS

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -22,6 +22,6 @@ export class NavComponent {
   logout(): void {
     this.router.navigate(['login']);
     this.authService.logout();
-    this.toast.info('Logout realizado com sucesso', 'Logout', { timeOut: 7000 });
+    this.toast.info('Logout realizado com sucesso', 'Logout');
   }
 }
